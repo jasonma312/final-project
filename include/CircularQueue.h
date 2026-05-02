ef0068fc947e98f8df2e476b8ce65727d38336bf
pmashi <peihuanma@gmail.com>
2026-05-01 20:39:49 -0400

    Circular Queue

--- include/CircularQueue.h
+++ include/CircularQueue.h
@@ -1,26 +1,33 @@
 #ifndef CIRCULARQUEUE_H
 #define CIRCULARQUEUE_H
 
-//fixed-size circular queue
-//use to compute N-day moving averages
 class CircularQueue {
 private:
     double* buffer;
-    int capacity;
-    int head;
-    int tail;
-    int count;
+    int     capacity;
+    int     head;
+    int     tail;
+    int     count;
+    double  sum;        // variable used to make getAverage() time complexity to O(1)
 
 public:
     CircularQueue(int capacity);
     ~CircularQueue();
-
+    // Adds a value. If Queue is full, overwrites oldest value with new
+    // and makes new head the next oldest value
     void enqueue(double value);
+    
+    // Removes and returns the oldest value. 
     double dequeue();
+
+    // Return the oldest value without remove it. 
     double peek() const;    //oldest value
+    
     double getAverage() const;
+    
     bool isFull()  const;
     bool isEmpty() const;
+
     int getCount() const;   //number of elements currently in queue
     int getCapacity() const; //max number of elements queue can hold
 };
---
+++ src/CircularQueue.cpp
@@ -0,0 +1,61 @@
+#include "CircularQueue.h"
+#include <stdexcept>
+
+CircularQueue::CircularQueue(int capacity) 
+: capacity(capacity), head(0), tail(0), count(0), sum(0.0) { 
+    buffer = new double[capacity];
+}
+
+CircularQueue::~CircularQueue() { 
+    delete[] buffer;
+}
+
+void CircularQueue::enqueue(double value) {
+    if(isFull()) { 
+        sum -= buffer[head];            // overwrites oldest element 
+        head = (head + 1) % capacity;   // shifts oldest element to next
+    } else { 
+        count ++; 
+    }
+    sum += value; 
+    buffer[tail] = value; 
+    tail = (tail + 1) % capacity; 
+}
+
+double CircularQueue::dequeue() { 
+    if(isEmpty()) { 
+        throw std::runtime_error("Queue is empty");
+    }
+    double value = buffer[head];    // return 
+    sum -= value;                   // update running sum 
+
+    head = (head + 1) % capacity; 
+    count--; 
+    return value; 
+}
+
+double CircularQueue::peek() const {
+    if(isEmpty()) throw std::runtime_error("Queue is empty");
+    return buffer[head]; 
+}
+
+double CircularQueue::getAverage() const {
+    if(isEmpty()) return 0.0; 
+    return sum / count; 
+}
+
+bool CircularQueue::isFull() const { 
+    return count == capacity; 
+}
+
+bool CircularQueue::isEmpty() const { 
+    return count == 0;
+}
+
+int CircularQueue::getCount() const { 
+    return count; 
+}
+
+int CircularQueue::getCapacity() const { 
+    return capacity; 
+}
